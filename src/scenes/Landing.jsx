4223cbac9121a351e196b787cf922ac3870aeef7
Michael Liu <michael.liu.mike@gmail.com>
2023-04-01 22:09:02 -0700

    added pfp

---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,44 @@
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import pfp from '../assets/pfp.jpeg';
+
+
+const Landing = ({setSelectedPage}) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+    return (
+        <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+
+            {/* PFP SECTION */}
+            <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+                {isAboveMediumScreens ? (
+                    <div
+                        className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+                        before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+                    >
+                        <img 
+                            alt="profile"
+                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]"
+                            src={pfp}
+                        />
+                    </div>
+                ) : (
+                    <img 
+                            alt="profile"
+                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]"
+                            src={pfp}
+                        />
+                )}
+
+                {/* MAIN SECTION */}
+                
+            </div>
+        </section>
+    )
+}
+
+
+
+
+export default Landing;
