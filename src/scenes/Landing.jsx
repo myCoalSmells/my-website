652e0bfe7767dcbd24080016702983bf39b0d05a
Michael Liu <michael.liu.mike@gmail.com>
2023-04-01 22:39:44 -0700

    added contact me and chat with me buttons

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -15,18 +15,18 @@ const Landing = ({setSelectedPage}) => {
                 {isAboveMediumScreens ? (
                     <div
                         className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
-                        before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+                        before:w-full before:max-w-[400px] md:before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]"
                     >
                         <img 
                             alt="profile"
-                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]"
+                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[400px] rounded-t-[400px]"
                             src={pfp}
                         />
                     </div>
                 ) : (
                     <img 
                             alt="profile"
-                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]"
+                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[400px] rounded-t-[400px]"
                             src={pfp}
                         />
                 )}
@@ -57,6 +57,37 @@ const Landing = ({setSelectedPage}) => {
                     <p className="mt-10 mb-7 text-sm text-center md:text-start">
                         Hello! Nice to meet u!
                     </p>
+                </motion.div>
+
+                {/* CALL TO ACTIONS */}
+                <motion.div
+                    className="flex mt-5 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5}}
+                    transition={{ delay: 0.2, duration: 0.7}}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0}
+                    }}
+                >
+                    <AnchorLink
+                        className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
+                        onClick={() => setSelectedPage("contact")}
+                    >
+                        Contact me!
+                    </AnchorLink>
+
+                    <AnchorLink
+                        className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                        onClick={() => setSelectedPage("contact")}
+                    >
+                        <div
+                            className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair px-10"
+                        >
+                            Let's chat!
+                        </div>
+                    </AnchorLink>
                 </motion.div>
             </div>
         </section>
