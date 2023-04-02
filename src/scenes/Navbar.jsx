df9404736664523ee4b78200afe7f8cef631e879
Michael Liu <michael.liu.mike@gmail.com>
2023-04-01 21:38:15 -0700

    added dots to the right of screen

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Link = ({page, selectedPage, setSelectedPage}) =>{
     const lowerCasePage = page.toLowerCase();
@@ -18,12 +19,13 @@ const Link = ({page, selectedPage, setSelectedPage}) =>{
     )
 }
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+    const navBarBackground = isTopOfPage ? "" : "bg-red";
 
     return(
-        <nav className={'z-40 w-full fixed top-0 py-6'}>
+        <nav className={`${navBarBackground}z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-playfair text-3xl font-bold">ML</h4>
                 {/* Desktop NAV */}
@@ -57,6 +59,42 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
                     >
                         <MenuIcon />
                     </button>
+                    )}
+
+                    {/* Mobile MENU POPUP */}
+                    {!isAboveSmallScreens && isMenuToggled && (
+                        <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+                            {/* CLOSE ICON */}
+                            <div className="flex justify-end p-12">
+                                <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                    <CloseIcon />
+                                </button>
+                            </div>
+
+                            {/* MENU ITEMS */}
+                            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                                <Link 
+                                    page="Home"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link 
+                                    page="Skills"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link 
+                                    page="Projects"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link 
+                                    page="Contact"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                            </div>
+                        </div>
                     )}
             </div>
         </nav>
