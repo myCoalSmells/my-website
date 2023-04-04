3cfa2bbc1f45faff82d7225d6f3bb7c18c205417
Michael Liu <michael.liu.mike@gmail.com>
2023-04-04 11:46:10 -0700

    new resume

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -25,7 +25,7 @@ const SocialMediaIcons = () => {
 
             <a
                 className="hover:opactiy-50 transition duration-500"
-                href="https://drive.google.com/file/d/1qroJgQ_xGqdz3qfPU_cVAuh9YUptda-V/view?usp=sharing"
+                href="https://drive.google.com/file/d/1RRYsaaDuqL_yzewjcbwDPR0zZABql0-E/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
             >
